8ecc5d2b3fe8573f09f1dc048a36417ebcc3bffe
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-10 09:48:57 +0200

    Solve 14

---
+++ A-Beginner/14/script.js
@@ -0,0 +1,7 @@
+function add(a, b, c) {
+    return a + b + c
+}
+
+console.log(add(3, 7, 10, 11))
+
+// Functionen kommer inte plussa ihop 11:n eftersom att den bara accepterar tre argument, a, b, och c
