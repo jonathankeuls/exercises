7e1c78a5f103c11559cc87ff91e9d5b2a9096da7
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-14 13:10:19 +0200

    Solve 23

--- A-Beginner/23/script.js
+++ A-Beginner/23/script.js
@@ -1,5 +1,6 @@
 function isDivisibleByFive(num) {
     num = prompt('Is the number divisible by 5?')
+    // Man använder === om man vill vet om ett tal är delbart med 5 i detta fall. == om man vill veta om ett tal är even eller odd
     if(num % 5 === 0) {
         return true
     } else {
