27d63885180bc01cd8c8d85d40ba203adceccfed
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-14 12:25:32 +0200

    Change 22

---
+++ A-Beginner/22/script.js
@@ -0,0 +1,8 @@
+function isOdd(num) {
+    num = prompt('Is the number odd?')
+    if (num % 1 == 0) {
+        return true
+    } else {
+        return false
+    }
+}
---
+++ A-Beginner/23/script.js
@@ -0,0 +1,9 @@
+function isDivisibleByFive(num) {
+    num = prompt('Is the number divisible by 5?')
+    if(num % 5 === 0) {
+        return true
+    } else {
+        return false
+    }
+
+}
