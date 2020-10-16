71fd4bc40c04e8bae9304e50936a9e1caa8e561a
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-16 15:48:37 +0200

    Added comment on C5

--- C-More/5/script.js
+++ C-More/5/script.js
@@ -1,10 +1,13 @@
 function filterOdds(myArray) {
     myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
+// Här lägger jag in en annan function som bara returner tal som är jämna    
     return myArray.filter(filterOdds2) 
 }
 
 function filterOdds2(x) {
     return x % 2 == 0
 }
+
+
 console.log(filterOdds())
