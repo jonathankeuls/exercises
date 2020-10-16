ec022c91809112d60c72f7f9f97744d4f07176ea
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-16 14:56:39 +0200

    Solve C3

---
+++ C-More/3/script.js
@@ -0,0 +1,8 @@
+function isSameAge(person1, person2) {
+    person1 = {name: "Johan", age: 39}
+    person2 = {name: "Peter", age: 41}
+
+    return person1.age === person2.age
+}
+
+console.log(isSameAge())
