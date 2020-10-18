b33bd9197f339f460d42506b89ef4876bc361f0f
jonathankeuls <jonathan.keuls@skola.botkyrka.se>
2020-10-18 11:44:25 +0200

    New solve C10

--- C-More/10/script.js
+++ C-More/10/script.js
@@ -1,1 +1,13 @@
-function makePerson()
+function makePerson(firstName, lastName) {
+    return {
+        firstname: 'firstName',
+        lastName: 'lastName',
+        isHuman: true
+    }
+}
+
+const person1 = makePerson('Kalle', 'Johansson')
+const person2 = makePerson('Linn', 'Persson')
+
+console.log(person1)
+console.log(person2)
